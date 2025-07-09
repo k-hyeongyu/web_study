d83969a46c2593f956155edd70ca6cbce4e2b92a
k-hyeongyu <kampher@naver.com>
2025-07-09 15:08:52 +0900

    연습1 URL 맵핑

---
+++ src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller //어노테이션
+public class HomeController {
+
+//	@RequestMapping("/url1")
+//	public String url1() {
+//		return "url1";
+//	}
+//
+//	@RequestMapping("/url2")
+//	public String url2() {
+//		return "url2"; 
+//	}
+//
+//	@RequestMapping("/url3")
+//	public String url3() {
+//		return "url3";
+//	}
+		//return "home";
+	}
