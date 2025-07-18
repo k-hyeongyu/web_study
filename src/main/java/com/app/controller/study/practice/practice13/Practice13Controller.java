5c9fbf7f941b0e0b2b549bdb39ff5b63c00b7b90
k-hyeongyu <kampher@naver.com>
2025-07-18 15:58:59 +0900

    연습13 쿠키 활용 광고 안보기

--- src/main/java/com/app/controller/study/practice/practice13/Practice13Controller.java
+++ src/main/java/com/app/controller/study/practice/practice13/Practice13Controller.java
@@ -12,29 +12,32 @@ import com.app.util.MyCookieUtil;
 
 @Controller
 public class Practice13Controller {
+
 	
 	@GetMapping("/practice13/hideAd")
-	public String hideAd(HttpServletResponse request){
+	public String hideAd(HttpServletRequest request) {
 		
 		String hideAd = MyCookieUtil.getCookie(request, "hideAd");
 		if(hideAd != null) {
 			request.setAttribute("hideAd", hideAd);
 		}
+		
 		return "practice/practice13/hideAd";
 	}
 	
-	
 	@PostMapping("/practice13/hideAd")
-	public String hideAdAction(HttpServletResponse response, HttpServletRequest request){
+	public String hideAdAction(HttpServletRequest request, HttpServletResponse response) {
 		
 		System.out.println(request.getParameter("hideAd"));
+		//24시간보지않기 체크후 요청을 했으면?
+		//쿠키에 값 저장!! -> 나중에 해당 화면 접근시, 광고 표시 여부로 활용
 		
-		if(request.getParameter("hideAd") != null) {
+		if(request.getParameter("hideAd") != null) {  //체크함 확인~
 			Cookie ck = MyCookieUtil.createCookie("hideAd", "hideAd", 60 * 60 * 24);
 			response.addCookie(ck);
 		}
 		
+		
 		return "redirect:/practice13/hideAd";
 	}
-
 }
