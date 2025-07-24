f7b0cb490b9746c9ea3eb24613d33b7091bc9b6a
k-hyeongyu <kampher@naver.com>
2025-07-24 11:33:43 +0900

    세션 및 로그인 관련 처리 담당 유틸성 클래스 추가함

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import com.app.common.CommonCode;
 import com.app.dto.user.User;
 import com.app.service.user.UserService;
+import com.app.util.LoginManager;
 
 @Controller
 public class CustomerController {
@@ -65,7 +66,8 @@ public class CustomerController {
 			System.out.println("로그인 성공");
 
 			// 현재 로그인 성공한 사용자 아이디 -> session 영역에 저장
-			session.setAttribute("loginUserId", loginUser.getId());
+			// session.setAttribute("loginUserId", loginUser.getId());
+			LoginManager.setSessionLoginUser(session, loginUser.getId());
 
 			// return "redirect:/customer/mypage"; //로그인 성공한 후, 마이페이지로 이동
 			return "redirect:/main"; // 로그인 성공한 후, 메인 페이지로
@@ -78,10 +80,12 @@ public class CustomerController {
 
 		// 로그인을 했으면, 로그인 한 사용자의 정보를 보여주는 마이페이지
 
-		if (session.getAttribute("loginUserId") != null) { // 로그인이 된 상태
+		// if (session.getAttribute("loginUserId") != null) { // 로그인이 된 상태
+		if (LoginManager.isLogin(session)) {
 
-			String loginUserId = (String) session.getAttribute("loginUserId");
-
+			//String loginUserId = (String) session.getAttribute("loginUserId");
+			String loginUserId = LoginManager.getLoginUserId(session);
+			
 			// 로그인 된 사용자 ID (세션에 저장되어있음)
 			User user = userService.findUserById(loginUserId);
 			// 그 ID에 해당하는 사용자 정보 -> view 전달
@@ -98,51 +102,43 @@ public class CustomerController {
 	public String logout(HttpSession session) {
 
 		// 세션 초기화
-		session.invalidate();
-
+		//session.invalidate();
+		LoginManager.logout(session);
+		
+		
 		return "redirect:/main";
 	}
-	
+
 	@GetMapping("/customer/modifyPw")
 	public String modifyPw() {
 		return "customer/modifyPw";
 	}
-	
+
 	@PostMapping("/customer/modifyPw")
 	public String modifyPwAction(User user, HttpSession session) {
 
-		//세션 로그인 사용자 ID
-		//화면에서 전달된 변경할 pw -> User 
-		
-		// 비밀번호 변경 처리 (user)   Service -> DAO -> mapper sql 
+		// 세션 로그인 사용자 ID
+		// 화면에서 전달된 변경할 pw -> User
+
+		// 비밀번호 변경 처리 (user) Service -> DAO -> mapper sql
 		// set pw = ?
 		// where id = ?
-		
+
 		// User 전체 update -> 기존정보는 유지 + 변경할 비밀번호
-		
+
 		// 특정 id의 비밀번호만 update
-		user.setId((String)session.getAttribute("loginUserId"));
+		//user.setId((String) session.getAttribute("loginUserId"));
+		user.setId( LoginManager.getLoginUserId(session));
 		
 		System.out.println("비밀번호 변경에 사용할 user 객체");
 		System.out.println(user);
-		
+
 		int result = userService.modifyUserPw(user);
-		
-		if(result > 0) {  //성공
+
+		if (result > 0) { // 성공
 			return "redirect:/customer/mypage";
-		} else { //실패
+		} else { // 실패
 			return "customer/modifyPw";
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
