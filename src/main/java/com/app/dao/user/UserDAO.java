6f741475bc02c79105c435ec659d6ca1423f8a3b
k-hyeongyu <kampher@naver.com>
2025-07-24 17:14:06 +0900

    관리자 페이지 - 사용자 검색기능 추가 (조건에 따라 검색, Mybatis 동적 쿼리 생성)

--- src/main/java/com/app/dao/user/UserDAO.java
+++ src/main/java/com/app/dao/user/UserDAO.java
@@ -3,6 +3,7 @@ package com.app.dao.user;
 import java.util.List;
 
 import com.app.dto.user.User;
+import com.app.dto.user.UserSearchCondition;
 
 public interface UserDAO {
 
@@ -18,4 +19,5 @@ public interface UserDAO {
 	
 	public int modifyUser(User user);
 	
+	public List<User> findUserListBySearchCondition(UserSearchCondition userSearchCondition);
 }
---
+++ src/main/java/com/app/dto/user/UserSearchCondition.java
@@ -0,0 +1,13 @@
+package com.app.dto.user;
+
+import lombok.Data;
+
+@Data
+public class UserSearchCondition {
+
+	String id;
+	String searchKeyword;
+	String userType;
+	String name;
+	
+}
